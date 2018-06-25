426f34698c2bc7d16efc7376e0a1d8aa287ff97f
Nikola Tasic <ntasic7@gmail.com>
2018-06-25 23:17:18 +0200

    works?

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const program = require('commander');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const program = require("commander");
+const axios_1 = require("axios");
 // program
 // 	.version('0.1.0')
 // 	.option('-d, --deploy <repo>', 'Deploy')
@@ -9,113 +10,87 @@ const bodyParser = require('body-parser');
 // 	.option('-r, --run <string>', 'Run')
 // 	.option('-f, --find <string>', 'Find')
 // 	.parse(process.argv);
-
 // if (program.deploy) console.log(' int: %j', program.integer);
 // if (program.kill) console.log('kill');
 // if (program.update) console.log('update');
 // if (program.run) console.log('run');
 // if (program.find) console.log('find');
-
+const serverUrl = 'http://localhost:3000';
 program
-	.version('0.1.0')
-	.command('deploy <repo>')
-	.action(repo => {
-		if (repo) deploy(repo);
-		else console.log('Invalid repository');
-	});
+    .version('0.1.0')
+    .command('deploy <repo>')
+    .action(repo => {
+    execute({
+        path: 'deploy',
+        data: {
+            repository: repo
+        }
+    });
+});
 program
-	.version('0.1.0')
-	.command('kill <name>')
-	.action(name => {
-		if (name) kill(name);
-		else console.log('Invalid name');
-	});
+    .version('0.1.0')
+    .command('kill [name]')
+    .action(name => {
+    execute({
+        path: 'kill',
+        data: {
+            query: name
+        }
+    });
+});
 program
-	.version('0.1.0')
-	.command('update <name>')
-	.action(name => {
-		if (name) update(name);
-		else console.log('Invalid name');
-	});
-
+    .version('0.1.0')
+    .command('update [name]')
+    .action(name => {
+    execute({
+        path: 'update',
+        data: {
+            query: name
+        }
+    });
+});
 program
-	.version('0.1.0')
-	.command('run <name>')
-	.action(name => {
-		if (name) run(name);
-		else console.log('Invalid name');
-	});
+    .version('0.1.0')
+    .command('run [name]')
+    .action(name => {
+    execute({
+        path: 'run',
+        data: {
+            query: name
+        }
+    });
+});
 program
-	.version('0.1.0')
-	.command('find <name>')
-	.action(name => {
-		if (name) find(name);
-		else console.log('Invalid name');
-	});
-
-async function deploy(repo) {
-	console.log(name);
-	await axios
-		.post('http://localhost:3000/deploy', {
-			repository: repo
-		})
-		.then(function(response) {
-			console.log(response.data);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-}
-async function kill(name) {
-	console.log(name);
-	await axios
-		.post('http://localhost:3000/kill', {
-			query: name
-		})
-		.then(function(response) {
-			console.log(response.data);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-}
-async function update(name) {
-	console.log(name);
-	await axios
-		.post('http://localhost:3000/update', {
-			query: name
-		})
-		.then(function(response) {
-			console.log(response.data);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-}
-async function run(name) {
-	console.log(name);
-	await axios
-		.post('http://localhost:3000/run', {
-			query: name
-		})
-		.then(function(response) {
-			console.log(response.data);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-}
-async function find(name) {
-	console.log(name);
-	await axios
-		.post('http://localhost:3000/find', {
-			query: name
-		})
-		.then(function(response) {
-			console.log(response.data);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
+    .version('0.1.0')
+    .command('find [name]')
+    .action(name => {
+    execute({
+        path: 'find',
+        data: {
+            query: name
+        }
+    });
+});
+program
+    .version('0.1.0')
+    .command('clear [name]')
+    .action(name => {
+    execute({
+        path: 'clear',
+        data: {
+            query: name
+        }
+    });
+});
+function execute(payload) {
+    axios_1.default({
+        method: 'post',
+        url: `${serverUrl}/${payload.path}`,
+        data: payload.data
+    })
+        .then(function (response) {
+        console.log(response.data);
+    })
+        .catch(err => console.log(err));
 }
 program.parse(process.argv);
